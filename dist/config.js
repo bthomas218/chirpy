296429e3b70b69f7108b723c36ba9e5308f50e31
bthomas218 <bradythomas690@gmail.com>
2025-11-27 13:06:56 -0600

    feat: added drizzle ORM and users table

--- dist/config.js
+++ dist/config.js
@@ -1,4 +1,13 @@
+process.loadEnvFile();
+function envOrThrow(key) {
+    const value = process.env[key];
+    if (!value) {
+        throw new Error(`${key} is missing from .env file!`);
+    }
+    return value;
+}
 const config = {
     fileserverHits: 0,
+    dbURL: envOrThrow("DB_URL"),
 };
 export default config;
--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,21 @@
+process.loadEnvFile();
+
 type APIConfig = {
   fileserverHits: number;
+  dbURL: string;
 };
+
+function envOrThrow(key: string) {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`${key} is missing from .env file!`);
+  }
+  return value;
+}
 
 const config: APIConfig = {
   fileserverHits: 0,
+  dbURL: envOrThrow("DB_URL"),
 };
 
 export default config;
